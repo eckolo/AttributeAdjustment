9794e504d0c5c258eeb4b045296747501e5735e8
eckolo <eckolo.0722@gmail.com>
2019-01-23 02:03:51 +0900

    属性列挙型作成

---
+++ Assets/Src/Domain/Model/Value/Energy.cs
@@ -0,0 +1,65 @@
+﻿namespace Assets.Src.Domain.Model.Value
+{
+    /// <summary>
+    /// 属性種別
+    /// </summary>
+    public enum Energy
+    {
+        /// <summary>
+        /// 炎
+        /// </summary>
+        FLAME,
+        /// <summary>
+        /// 氷
+        /// </summary>
+        ICE,
+        /// <summary>
+        /// 風
+        /// </summary>
+        WIND,
+        /// <summary>
+        /// 重
+        /// </summary>
+        GRAVITY,
+        /// <summary>
+        /// 光
+        /// </summary>
+        LIGHT,
+        /// <summary>
+        /// 闇
+        /// </summary>
+        DARKNESS,
+        /// <summary>
+        /// 雷
+        /// </summary>
+        THUNDER,
+        /// <summary>
+        /// 地
+        /// </summary>
+        EARTH,
+        /// <summary>
+        /// 命
+        /// </summary>
+        LIFE,
+        /// <summary>
+        /// 毒
+        /// </summary>
+        POISON,
+        /// <summary>
+        /// 斬
+        /// </summary>
+        SLASH,
+        /// <summary>
+        /// 打
+        /// </summary>
+        BLOW,
+        /// <summary>
+        /// 衝
+        /// </summary>
+        IMPACT,
+        /// <summary>
+        /// 突
+        /// </summary>
+        PIERCING,
+    }
+}
