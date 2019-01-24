08b4672f48d591a22a6a5cfab06d88eef821fbae
eckolo <eckolo.0722@gmail.com>
2019-01-25 00:28:36 +0900

    モーションチップ定義

---
+++ Assets/Src/Domain/Model/Value/MotionTip.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Src.Domain.Model.Abstract;
+
+namespace Assets.Src.Domain.Model.Value
+{
+    /// <summary>
+    /// 部分動作を表すモーション
+    /// </summary>
+    public class MotionTip : Named
+    {
+        /// <summary>
+        /// 属性種別
+        /// </summary>
+        public Energy energy { get; }
+        /// <summary>
+        /// 属性値
+        /// </summary>
+        public int energyValue { get; }
+    }
+}
