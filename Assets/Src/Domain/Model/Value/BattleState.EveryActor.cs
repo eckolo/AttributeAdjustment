f110f56b156f3dbb4babc779f1a38be769d7ba7a
eckolo <eckolo.0722@gmail.com>
2019-01-27 17:51:22 +0900

    戦闘状態保持クラス定義

---
+++ Assets/Src/Domain/Model/Value/BattleState.EveryActor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Src.Domain.Model.Value
+{
+    public partial class BattleState
+    {
+        /// <summary>
+        /// 行動者毎の戦闘状態
+        /// </summary>
+        public class EveryActor
+        {
+            /// <summary>
+            /// プレイヤー操作対象であるか否かのフラグ
+            /// </summary>
+            public bool player;
+            /// <summary>
+            /// 懐札
+            /// </summary>
+            public IEnumerable<MotionTip> selfTip;
+            /// <summary>
+            /// 手札
+            /// </summary>
+            public IEnumerable<MotionTip> handTip;
+        }
+    }
+}
---
+++ Assets/Src/Domain/Model/Value/BattleState.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Src.Domain.Model.Entity;
+using System.Collections.Generic;
+
+namespace Assets.Src.Domain.Model.Value
+{
+    /// <summary>
+    /// 戦闘の状態保持クラス
+    /// </summary>
+    public partial class BattleState
+    {
+        /// <summary>
+        /// 山札
+        /// </summary>
+        public Queue<MotionTip> deckTip { get; protected set; }
+
+        /// <summary>
+        /// 場札
+        /// </summary>
+        public IEnumerable<MotionTip> boardTip { get; protected set; }
+
+        /// <summary>
+        /// 行動者毎の戦闘状態リスト情報
+        /// </summary>
+        public Dictionary<BattleActor, EveryActor> battleActorList { get; set; }
+    }
+}
