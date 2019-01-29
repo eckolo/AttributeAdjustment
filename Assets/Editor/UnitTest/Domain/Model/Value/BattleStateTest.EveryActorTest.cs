ae7085fe0ba10289ea65ce9f644ad8e64e59058a
eckolo <eckolo.0722@gmail.com>
2019-01-30 07:02:03 +0900

    手札取り出し処理実装

--- Assets/Editor/UnitTest/Domain/Model/Value/BattleStateTest.EveryActorTest.cs
+++ Assets/Editor/UnitTest/Domain/Model/Value/BattleStateTest.EveryActorTest.cs
@@ -138,6 +138,259 @@ namespace Assets.Editor.UnitTest.Domain.Model.Value
                     .Count(elem => elem.energyValue == tip2.energyValue)
                     .Is(1);
             }
+
+            [Test]
+            public static void PopHandTip_通常処理_全て取り出し成功()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tipList = new List<MotionTip> { tip1, tip1, tip1, tip2, tip2, tip2, tip3 };
+                var popList = new List<MotionTip> { tip1, tip1, tip1, tip2 };
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Count().Is(popList.Count);
+                result
+                    .Where(elem => elem.energy == tip1.energy)
+                    .Count(elem => elem.energyValue == tip1.energyValue)
+                    .Is(3);
+                result
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(1);
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Count().Is(tipList.Count - popList.Count);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(2);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip3.energy)
+                    .Count(elem => elem.energyValue == tip3.energyValue)
+                    .Is(1);
+            }
+            [Test]
+            public static void PopHandTip_通常処理_一部取り出し成功()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tipList = new List<MotionTip> { tip1, tip2, tip2, tip2 };
+                var popList = new List<MotionTip> { tip1, tip1, tip2, tip3 };
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Count().Is(2);
+                result
+                    .Where(elem => elem.energy == tip1.energy)
+                    .Count(elem => elem.energyValue == tip1.energyValue)
+                    .Is(1);
+                result
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(1);
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Count().Is(2);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(2);
+            }
+            [Test]
+            public static void PopHandTip_通常処理_全て取り出し失敗()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tip4 = MotionTipMock.Generate(Energy.FLAME, 60);
+                var tipList = new List<MotionTip> { tip2, tip2, tip4 };
+                var popList = new List<MotionTip> { tip1, tip1, tip1, tip3 };
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Count().Is(tipList.Count);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(2);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip4.energy)
+                    .Count(elem => elem.energyValue == tip4.energyValue)
+                    .Is(1);
+            }
+            [Test]
+            public static void PopHandTip_手札が空()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tip4 = MotionTipMock.Generate(Energy.FLAME, 60);
+                var tipList = new List<MotionTip>();
+                var popList = new List<MotionTip> { tip1, tip1, tip2, tip3 };
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Any().IsFalse();
+            }
+            [Test]
+            public static void PopHandTip_手札がNull()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tip4 = MotionTipMock.Generate(Energy.FLAME, 60);
+                var popList = new List<MotionTip> { tip1, tip1, tip2, tip3 };
+                var actorState = BattleStateMock.EveryActorMock.Generate(null);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNull();
+            }
+            [Test]
+            public static void PopHandTip_取り出しリストが空()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tipList = new List<MotionTip> { tip1, tip1, tip1, tip2, tip2, tip3 };
+                var popList = new List<MotionTip>();
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Count().Is(tipList.Count);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip1.energy)
+                    .Count(elem => elem.energyValue == tip1.energyValue)
+                    .Is(3);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(2);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip3.energy)
+                    .Count(elem => elem.energyValue == tip3.energyValue)
+                    .Is(1);
+            }
+            [Test]
+            public static void PopHandTip_取り出しリストがNull()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tipList = new List<MotionTip> { tip1, tip1, tip1, tip2, tip2, tip3 };
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(null);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Count().Is(tipList.Count);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip1.energy)
+                    .Count(elem => elem.energyValue == tip1.energyValue)
+                    .Is(3);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip2.energy)
+                    .Count(elem => elem.energyValue == tip2.energyValue)
+                    .Is(2);
+                actorState.handTips
+                    .Where(elem => elem.energy == tip3.energy)
+                    .Count(elem => elem.energyValue == tip3.energyValue)
+                    .Is(1);
+            }
+            [Test]
+            public static void PopHandTip_手札が空_取り出しリストが空()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tipList = new List<MotionTip>();
+                var popList = new List<MotionTip>();
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Any().IsFalse();
+            }
+            [Test]
+            public static void PopHandTip_手札が空_取り出しリストがNull()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var tipList = new List<MotionTip>();
+                var actorState = BattleStateMock.EveryActorMock.Generate(tipList);
+
+                var result = actorState.PopHandTip(null);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNotNull();
+                actorState.handTips.Any().IsFalse();
+            }
+            [Test]
+            public static void PopHandTip_手札がNull_取り出しリストが空()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var popList = new List<MotionTip>();
+                var actorState = BattleStateMock.EveryActorMock.Generate(null);
+
+                var result = actorState.PopHandTip(popList);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNull();
+            }
+            [Test]
+            public static void PopHandTip_手札がNull_取り出しリストがNull()
+            {
+                var tip1 = MotionTipMock.Generate(Energy.DARKNESS, 10);
+                var tip2 = MotionTipMock.Generate(Energy.LIFE, 40);
+                var tip3 = MotionTipMock.Generate(Energy.WIND, 20);
+                var actorState = BattleStateMock.EveryActorMock.Generate(null);
+
+                var result = actorState.PopHandTip(null);
+
+                result.IsNotNull();
+                result.Any().IsFalse();
+
+                actorState.handTips.IsNull();
+            }
         }
     }
 }
--- Assets/Src/Domain/Model/Value/BattleState.EveryActor.cs
+++ Assets/Src/Domain/Model/Value/BattleState.EveryActor.cs
@@ -41,6 +41,33 @@ namespace Assets.Src.Domain.Model.Value
                 handTips = (handTips ?? empties).Concat(addedTips ?? empties).ToList();
                 return this;
             }
+            /// <summary>
+            /// 手札の取り出し
+            /// </summary>
+            /// <param name="popTips">取り出すモーションチップ一覧</param>
+            /// <returns>取り出しに成功したモーションチップの一覧</returns>
+            public IEnumerable<MotionTip> PopHandTip(IEnumerable<MotionTip> popTips)
+            {
+                var popMap = popTips?.GroupBy(tip => tip).ToDictionary(tip => tip.Key, tip => tip.Count());
+                var handTipMap = handTips?.GroupBy(tip => tip).ToDictionary(tip => tip.Key, tip => tip.Count());
+
+                handTips = handTipMap?
+                    .Select(tip => (tip: tip.Key, number: tip.Value - popMap.GetOrDefault(tip.Key, 0)))
+                    .Where(data => data.number > 0)
+                    .SelectMany(data => Enumerable.Range(0, data.number).Select(_ => data.tip))
+                    .ToList();
+
+                var popedTips = popMap?
+                    .Select(tip => (
+                        tip: tip.Key,
+                        number: Mathf.Min(tip.Value, handTipMap.GetOrDefault(tip.Key, 0))))
+                    .Where(data => data.number > 0)
+                    .SelectMany(data => Enumerable.Range(0, data.number).Select(_ => data.tip))
+                    .ToList()
+                    ?? new List<MotionTip>();
+
+                return popedTips;
+            }
         }
     }
 }
