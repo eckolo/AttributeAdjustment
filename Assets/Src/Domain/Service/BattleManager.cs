92c013e20f9de2eae229a824a7b2c6159df65948
eckolo <eckolo.0722@gmail.com>
2019-02-06 23:45:20 +0900

    ひとまず戦闘開始処理を確定

---
+++ Assets/Src/Domain/Service/BattleManager.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Src.Domain.Model.Value;
+
+namespace Assets.Src.Domain.Service
+{
+    /// <summary>
+    /// 戦闘処理制御に関連する処理サービス
+    /// </summary>
+    public static class BattleManager
+    {
+        /// <summary>
+        /// 戦闘開始処理
+        /// </summary>
+        /// <param name="state">戦闘開始時の戦闘状態</param>
+        /// <returns>戦闘開始処理後の戦闘状態</returns>
+        public static BattleState BattleStart(this BattleState state)
+        {
+            return state.SetupDeck().SetupBoard().SetupAllHandTips();
+        }
+    }
+}
