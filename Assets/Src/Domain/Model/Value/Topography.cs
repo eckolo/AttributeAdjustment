18682e6dad7b8fc97d72951d4e84017d81a19302
eckolo <eckolo.0722@gmail.com>
2019-02-17 23:40:07 +0900

    戦闘地形クラスを新規定義

---
+++ Assets/Src/Domain/Model/Value/Topography.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Src.Domain.Model.Value
+{
+    /// <summary>
+    /// 地形クラス
+    /// </summary>
+    public class Topography
+    {
+        public Topography(Dictionary<MotionTip, int> baseTipSet)
+        {
+            this.baseTipSet = baseTipSet ?? new Dictionary<MotionTip, int>();
+        }
+
+        /// <summary>
+        /// 地形の基礎モーションチップリスト
+        /// </summary>
+        public Dictionary<MotionTip, int> baseTipSet { get; }
+    }
+}
