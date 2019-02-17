792d8ef3a26e32fc53d4f186f29f5eb97607ffa5
eckolo <eckolo.0722@gmail.com>
2019-02-17 23:41:12 +0900

    仮置きで戦闘状態生成クラス作成

---
+++ Assets/Src/Domain/Factory/BattleStateFactory.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Src.Domain.Model.Entity;
+using Assets.Src.Domain.Model.Value;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Src.Domain.Factory
+{
+    public static class BattleStateFactory
+    {
+        public static BattleState ToBattleState(
+            this GameState state,
+            IEnumerable<Actor> enemys,
+            Topography topography)
+        {
+            var actiors = enemys.Concat(new[] { state.player });
+
+            var battleState = new BattleState(actiors, topography.baseTipSet);
+
+            return battleState;
+        }
+    }
+}
