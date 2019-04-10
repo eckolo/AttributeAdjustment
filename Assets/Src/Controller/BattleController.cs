bcdaab0c31e958963f0304e7c50015736e0295c4
eckolo <eckolo.0722@gmail.com>
2019-04-10 22:53:00 +0900

    戦闘処理統括をコントローラに移動

--- Assets/Src/Controller/BattleController.cs
+++ Assets/Src/Controller/BattleController.cs
@@ -1,15 +1,16 @@
 ﻿using Assets.Src.Domain.Factory;
 using Assets.Src.Domain.Model.Entity;
 using Assets.Src.Domain.Model.Value;
+using Assets.Src.Domain.Service;
 using System.Collections.Generic;
 using UniRx.Async;
 
-namespace Assets.Src.Domain.Service
+namespace Assets.Src.Controller
 {
     /// <summary>
     /// 戦闘処理制御に関連する処理サービス
     /// </summary>
-    public static class BattleManager
+    public static class BattleController
     {
         /// <summary>
         /// タイムライン上の戦闘制御処理
