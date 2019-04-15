41fbeb203b7f9dce28e24115e175d3aa2dc25d70
eckolo <eckolo.0722@gmail.com>
2019-04-16 00:51:57 +0900

    Energyオブジェクトのプロパティサービスの実装方法を変更

--- Assets/Editor/UnitTest/Domain/Service/EnergyPropertiesTest.cs
+++ Assets/Editor/UnitTest/Domain/Service/EnergyPropertiesTest.cs
@@ -2,6 +2,7 @@
 using Assets.Src.Domain.Service;
 using NUnit.Framework;
 using System;
+using UnityEngine;
 
 namespace Assets.Editor.UnitTest.Domain.Service
 {
@@ -13,20 +14,20 @@ namespace Assets.Editor.UnitTest.Domain.Service
         [Test]
         public static void GetNameTest_正常系()
         {
-            Energy.FLAME.GetName().Is(Constants.Energy.Name.FLAME);
-            Energy.ICE.GetName().Is(Constants.Energy.Name.ICE);
-            Energy.WIND.GetName().Is(Constants.Energy.Name.WIND);
-            Energy.GRAVITY.GetName().Is(Constants.Energy.Name.GRAVITY);
-            Energy.LIGHT.GetName().Is(Constants.Energy.Name.LIGHT);
-            Energy.DARKNESS.GetName().Is(Constants.Energy.Name.DARKNESS);
-            Energy.THUNDER.GetName().Is(Constants.Energy.Name.THUNDER);
-            Energy.EARTH.GetName().Is(Constants.Energy.Name.EARTH);
-            Energy.LIFE.GetName().Is(Constants.Energy.Name.LIFE);
-            Energy.POISON.GetName().Is(Constants.Energy.Name.POISON);
-            Energy.SLASH.GetName().Is(Constants.Energy.Name.SLASH);
-            Energy.BLOW.GetName().Is(Constants.Energy.Name.BLOW);
-            Energy.IMPACT.GetName().Is(Constants.Energy.Name.IMPACT);
-            Energy.PIERCING.GetName().Is(Constants.Energy.Name.PIERCING);
+            Energy.FLAME.GetName().Is("炎");
+            Energy.ICE.GetName().Is("氷");
+            Energy.WIND.GetName().Is("風");
+            Energy.GRAVITY.GetName().Is("重");
+            Energy.LIGHT.GetName().Is("光");
+            Energy.DARKNESS.GetName().Is("闇");
+            Energy.THUNDER.GetName().Is("雷");
+            Energy.EARTH.GetName().Is("地");
+            Energy.LIFE.GetName().Is("命");
+            Energy.POISON.GetName().Is("毒");
+            Energy.SLASH.GetName().Is("斬");
+            Energy.BLOW.GetName().Is("打");
+            Energy.IMPACT.GetName().Is("衝");
+            Energy.PIERCING.GetName().Is("突");
         }
         [Test]
         public static void GetNameTest_未定義()
@@ -38,20 +39,20 @@ namespace Assets.Editor.UnitTest.Domain.Service
         [Test]
         public static void GetColorTest_正常系()
         {
-            Energy.FLAME.GetColor().Is(Constants.Energy.Color.FLAME);
-            Energy.ICE.GetColor().Is(Constants.Energy.Color.ICE);
-            Energy.WIND.GetColor().Is(Constants.Energy.Color.WIND);
-            Energy.GRAVITY.GetColor().Is(Constants.Energy.Color.GRAVITY);
-            Energy.LIGHT.GetColor().Is(Constants.Energy.Color.LIGHT);
-            Energy.DARKNESS.GetColor().Is(Constants.Energy.Color.DARKNESS);
-            Energy.THUNDER.GetColor().Is(Constants.Energy.Color.THUNDER);
-            Energy.EARTH.GetColor().Is(Constants.Energy.Color.EARTH);
-            Energy.LIFE.GetColor().Is(Constants.Energy.Color.LIFE);
-            Energy.POISON.GetColor().Is(Constants.Energy.Color.POISON);
-            Energy.SLASH.GetColor().Is(Constants.Energy.Color.SLASH);
-            Energy.BLOW.GetColor().Is(Constants.Energy.Color.BLOW);
-            Energy.IMPACT.GetColor().Is(Constants.Energy.Color.IMPACT);
-            Energy.PIERCING.GetColor().Is(Constants.Energy.Color.PIERCING);
+            Energy.FLAME.GetColor().Is(new Color32(r: 255, g: 000, b: 000, a: 255));
+            Energy.ICE.GetColor().Is(new Color32(r: 000, g: 000, b: 255, a: 255));
+            Energy.WIND.GetColor().Is(new Color32(r: 000, g: 255, b: 000, a: 255));
+            Energy.GRAVITY.GetColor().Is(new Color32(r: 120, g: 120, b: 120, a: 255));
+            Energy.LIGHT.GetColor().Is(new Color32(r: 255, g: 255, b: 255, a: 255));
+            Energy.DARKNESS.GetColor().Is(new Color32(r: 000, g: 000, b: 000, a: 255));
+            Energy.THUNDER.GetColor().Is(new Color32(r: 255, g: 255, b: 000, a: 255));
+            Energy.EARTH.GetColor().Is(new Color32(r: 120, g: 000, b: 000, a: 255));
+            Energy.LIFE.GetColor().Is(new Color32(r: 000, g: 120, b: 000, a: 255));
+            Energy.POISON.GetColor().Is(new Color32(r: 120, g: 000, b: 120, a: 255));
+            Energy.SLASH.GetColor().Is(new Color32(r: 000, g: 255, b: 255, a: 255));
+            Energy.BLOW.GetColor().Is(new Color32(r: 120, g: 120, b: 000, a: 255));
+            Energy.IMPACT.GetColor().Is(new Color32(r: 000, g: 120, b: 120, a: 255));
+            Energy.PIERCING.GetColor().Is(new Color32(r: 000, g: 000, b: 120, a: 255));
         }
         [Test]
         public static void GetColorTest_未定義()
--- Assets/Src/Domain/Service/EnergyProperties.cs
+++ Assets/Src/Domain/Service/EnergyProperties.cs
@@ -1,4 +1,6 @@
 ﻿using Assets.Src.Domain.Model.Value;
+using System.Collections.Generic;
+using System.Linq;
 using System;
 using UnityEngine;
 
@@ -10,56 +12,58 @@ namespace Assets.Src.Domain.Service
     public static class EnergyProperties
     {
         /// <summary>
+        /// 名称
+        /// </summary>
+        static readonly Dictionary<Energy, string> nameMap = new Dictionary<Energy, string>
+        {
+            { Energy.FLAME, "炎" },
+            { Energy.ICE, "氷" },
+            { Energy.WIND, "風" },
+            { Energy.GRAVITY, "重" },
+            { Energy.LIGHT, "光" },
+            { Energy.DARKNESS, "闇" },
+            { Energy.THUNDER, "雷" },
+            { Energy.EARTH, "地" },
+            { Energy.LIFE, "命" },
+            { Energy.POISON, "毒" },
+            { Energy.SLASH, "斬" },
+            { Energy.BLOW, "打" },
+            { Energy.IMPACT, "衝" },
+            { Energy.PIERCING, "突" },
+        };
+        /// <summary>
         /// 名称取得
         /// </summary>
         /// <param name="energy">取得元オブジェクト</param>
         /// <returns>名称文字列</returns>
         public static string GetName(this Energy energy)
+            => nameMap.GetOrDefault(energy, null) ?? throw new ArgumentOutOfRangeException(nameof(energy));
+        /// <summary>
+        /// 対応カラー
+        /// </summary>
+        static readonly Dictionary<Energy, Color32?> colorMap = new Dictionary<Energy, Color32?>
         {
-            switch(energy)
-            {
-                case Energy.FLAME: return Constants.Energy.Name.FLAME;
-                case Energy.ICE: return Constants.Energy.Name.ICE;
-                case Energy.WIND: return Constants.Energy.Name.WIND;
-                case Energy.GRAVITY: return Constants.Energy.Name.GRAVITY;
-                case Energy.LIGHT: return Constants.Energy.Name.LIGHT;
-                case Energy.DARKNESS: return Constants.Energy.Name.DARKNESS;
-                case Energy.THUNDER: return Constants.Energy.Name.THUNDER;
-                case Energy.EARTH: return Constants.Energy.Name.EARTH;
-                case Energy.LIFE: return Constants.Energy.Name.LIFE;
-                case Energy.POISON: return Constants.Energy.Name.POISON;
-                case Energy.SLASH: return Constants.Energy.Name.SLASH;
-                case Energy.BLOW: return Constants.Energy.Name.BLOW;
-                case Energy.IMPACT: return Constants.Energy.Name.IMPACT;
-                case Energy.PIERCING: return Constants.Energy.Name.PIERCING;
-                default: throw new ArgumentOutOfRangeException(nameof(energy));
-            }
-        }
+            { Energy.FLAME, new Color32(r: 255, g: 000, b: 000, a: 255) },//赤
+            { Energy.ICE, new Color32(r: 000, g: 000, b: 255, a: 255) },//青
+            { Energy.WIND, new Color32(r: 000, g: 255, b: 000, a: 255) },//黄緑
+            { Energy.GRAVITY, new Color32(r: 120, g: 120, b: 120, a: 255) },//灰色
+            { Energy.LIGHT, new Color32(r: 255, g: 255, b: 255, a: 255) },//白
+            { Energy.DARKNESS, new Color32(r: 000, g: 000, b: 000, a: 255) },//黒
+            { Energy.THUNDER, new Color32(r: 255, g: 255, b: 000, a: 255) },//黄色
+            { Energy.EARTH, new Color32(r: 120, g: 000, b: 000, a: 255) },//茶色
+            { Energy.LIFE, new Color32(r: 000, g: 120, b: 000, a: 255) },//緑
+            { Energy.POISON, new Color32(r: 120, g: 000, b: 120, a: 255) },//紫
+            { Energy.SLASH, new Color32(r: 000, g: 255, b: 255, a: 255) },//水色
+            { Energy.BLOW, new Color32(r: 120, g: 120, b: 000, a: 255) },//黄土色
+            { Energy.IMPACT, new Color32(r: 000, g: 120, b: 120, a: 255) },//青緑
+            { Energy.PIERCING, new Color32(r: 000, g: 000, b: 120, a: 255) },//紺
+        };
         /// <summary>
         /// 色取得
         /// </summary>
         /// <param name="energy">取得元オブジェクト</param>
         /// <returns>名称文字列</returns>
         public static Color32 GetColor(this Energy energy)
-        {
-            switch(energy)
-            {
-                case Energy.FLAME: return Constants.Energy.Color.FLAME;
-                case Energy.ICE: return Constants.Energy.Color.ICE;
-                case Energy.WIND: return Constants.Energy.Color.WIND;
-                case Energy.GRAVITY: return Constants.Energy.Color.GRAVITY;
-                case Energy.LIGHT: return Constants.Energy.Color.LIGHT;
-                case Energy.DARKNESS: return Constants.Energy.Color.DARKNESS;
-                case Energy.THUNDER: return Constants.Energy.Color.THUNDER;
-                case Energy.EARTH: return Constants.Energy.Color.EARTH;
-                case Energy.LIFE: return Constants.Energy.Color.LIFE;
-                case Energy.POISON: return Constants.Energy.Color.POISON;
-                case Energy.SLASH: return Constants.Energy.Color.SLASH;
-                case Energy.BLOW: return Constants.Energy.Color.BLOW;
-                case Energy.IMPACT: return Constants.Energy.Color.IMPACT;
-                case Energy.PIERCING: return Constants.Energy.Color.PIERCING;
-                default: throw new ArgumentOutOfRangeException(nameof(energy));
-            }
-        }
+            => colorMap.GetOrDefault(energy, null) ?? throw new ArgumentOutOfRangeException(nameof(energy));
     }
 }
