68526a0f662df07a7418521e744fb803a00b5ae9
eckolo <eckolo.0722@gmail.com>
2019-09-26 02:16:18 +0900

    ビューのルート生成処理一通り実装

---
+++ Assets/Editor/UnitTest/View/Factory/ViewStateFactoryTest.cs
@@ -0,0 +1,109 @@
+﻿using Assets.Src.Domain.Model.Abstract;
+using Assets.Src.Mock;
+using Assets.Src.View.Factory;
+using Assets.Src.View.Model.Entity;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Editor.UnitTest.View.Factory
+{
+    /// <summary>
+    /// <see cref="ViewStateFactory"/>のテスト
+    /// </summary>
+    public static class ViewStateFactoryTest
+    {
+        static readonly ViewStateKey[] keys = new[]
+        {
+            ViewStateKeyMock.Generate(new[] { IViewKeyMock.Generate(0) }),
+            ViewStateKeyMock.Generate(new[] { IViewKeyMock.Generate(1) }),
+        };
+        static readonly ViewState[] states = new[]
+        {
+            ViewStateMock.Generate($"{nameof(ViewStateFactoryTest)}_1"),
+        };
+        [Test]
+        public static void GenerateViewStateTest_正常系_新規生成_既存無()
+        {
+            var viewRoot = new ViewRoot
+            {
+                viewStateMap = new Dictionary<ViewStateKey, ViewState>(),
+            };
+
+            var result = viewRoot.GenerateViewState(keys[0]);
+
+            result.IsNotNull();
+            viewRoot.viewStateMap.Count.Is(1);
+            viewRoot.viewStateMap.ContainsKey(keys[0]).IsTrue();
+            viewRoot.viewStateMap[keys[0]].IsSameReferenceAs(result);
+        }
+        [Test]
+        public static void GenerateViewStateTest_正常系_新規生成_既存有()
+        {
+            var viewRoot = new ViewRoot
+            {
+                viewStateMap = new Dictionary<ViewStateKey, ViewState>
+                {
+                    { keys[1], states[0] },
+                },
+            };
+
+            var result = viewRoot.GenerateViewState(keys[0]);
+
+            result.IsNotNull();
+            viewRoot.viewStateMap.Count.Is(2);
+            viewRoot.viewStateMap.ContainsKey(keys[0]).IsTrue();
+            viewRoot.viewStateMap[keys[0]].IsSameReferenceAs(result);
+        }
+        [Test]
+        public static void GenerateViewStateTest_正常系_既存取得()
+        {
+            var viewRoot = new ViewRoot
+            {
+                viewStateMap = new Dictionary<ViewStateKey, ViewState>
+                {
+                    { keys[1], states[0] },
+                },
+            };
+
+            var result = viewRoot.GenerateViewState(keys[1]);
+
+            result.IsNotNull();
+            result.Is(states[0]);
+            viewRoot.viewStateMap.Count.Is(1);
+            viewRoot.viewStateMap.ContainsKey(keys[1]).IsTrue();
+            viewRoot.viewStateMap[keys[1]].IsSameReferenceAs(result);
+        }
+        [Test]
+        public static void GenerateViewStateTest_異常系_RootがNull()
+        {
+            var viewRoot = (ViewRoot)null;
+
+            Assert.Throws<ArgumentNullException>(() => viewRoot.GenerateViewState(keys[0]));
+        }
+        [Test]
+        public static void GenerateViewStateTest_異常系_検索キーがNull()
+        {
+            var viewRoot = new ViewRoot
+            {
+                viewStateMap = new Dictionary<ViewStateKey, ViewState>(),
+            };
+            var nullKey = (ViewStateKey)null;
+
+            Assert.Throws<ArgumentNullException>(() => viewRoot.GenerateViewState(nullKey));
+        }
+        [Test]
+        public static void GenerateViewStateTest_異常系_仮想ツリーがNull()
+        {
+            var viewRoot = new ViewRoot
+            {
+                viewStateMap = null,
+            };
+
+            Assert.Throws<ArgumentNullException>(() => viewRoot.GenerateViewState(keys[0]));
+        }
+    }
+}
--- Assets/Src/View/Factory/ViewStateFactory.cs
+++ Assets/Src/View/Factory/ViewStateFactory.cs
@@ -1,5 +1,7 @@
-﻿using Assets.Src.Domain.Service;
+﻿using Assets.Src.Domain.Model.Abstract;
+using Assets.Src.Domain.Service;
 using Assets.Src.View.Model.Entity;
+using Assets.Src.View.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +14,35 @@ namespace Assets.Src.View.Factory
     public static class ViewStateFactory
     {
         /// <summary>
-        /// ビューの新規生成
+        /// <see cref="ViewState"/>の新規生成処理
+        /// 既に存在していた場合は取得
         /// </summary>
-        /// <param name="name">名称</param>
-        /// <param name="parent">親オブジェクト</param>
-        /// <returns>生成されたビュー</returns>
-        public static ViewState ToViewState(this string name, MonoBehaviour parent = null)
+        /// <typeparam name="TKey">生成元のキーの型</typeparam>
+        /// <param name="key">生成元のキー</param>
+        /// <returns>生成された<see cref="ViewState"/></returns>
+        public static ViewState GenerateViewState<TKey>(this ViewRoot viewRoot, TKey key) where TKey : ViewStateKey
         {
-            var _name = name is null ? "" : $"_{name}";
-            return new GameObject($"{nameof(ViewState)}{_name}", typeof(ViewState)).GetComponent<ViewState>().SetParent(parent);
+            if(viewRoot is null)
+                throw new ArgumentNullException(nameof(viewRoot));
+            if(key is null)
+                throw new ArgumentNullException(nameof(key));
+
+            var stateMap = viewRoot.viewStateMap;
+            var name = $"{nameof(ViewState)}_{key.GetType().FullName}";
+
+            if(stateMap is null)
+                throw new ArgumentNullException(nameof(viewRoot.viewStateMap));
+
+            lock(stateMap)
+            {
+                if(!stateMap.ContainsKey(key))
+                {
+                    var state = new GameObject(name, typeof(ViewState)).AddComponent<ViewState>();
+                    stateMap.Add(key, state);
+                }
+            }
+
+            return stateMap.GetOrDefault(key);
         }
     }
 }
