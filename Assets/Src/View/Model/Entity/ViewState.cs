8f687e2be225bdf91ab8f0fcea6482ef264611fe
eckolo <eckolo.0722@gmail.com>
2019-11-25 12:22:57 +0900

    ビュールートオブジェクト事態にリポジトリとしての機能を持たせてみる

--- Assets/Src/View/Model/Entity/ViewState.cs
+++ Assets/Src/View/Model/Entity/ViewState.cs
@@ -1,5 +1,9 @@
+using Assets.Src.Domain.Model.Abstract;
 using Assets.Src.Domain.Service;
 using Assets.Src.View.Model.Abstract;
+using Assets.Src.View.Repository;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Src.View.Model.Entity
@@ -8,7 +12,21 @@ namespace Assets.Src.View.Model.Entity
     /// ビュー類のルートになるオブジェクト
     /// それだけ
     /// </summary>
-    public class ViewState : PrefabAbst
+    public class ViewState : PrefabAbst, IComponentRepository
     {
+        readonly Dictionary<IViewKey, Component> _viewMap = new Dictionary<IViewKey, Component>();
+
+        public Component SearchView<TKey>(TKey key) where TKey : IViewKey
+            => key is TKey
+                ? _viewMap.GetOrDefault(key)
+                : default;
+        public Component SaveView<TKey>(TKey key, Component component) where TKey : IViewKey
+        {
+            if(_viewMap.ContainsKey(key))
+                return component;
+
+            _viewMap.Add(key, component);
+            return component;
+        }
     }
 }
---
+++ Assets/Src/View/Repository/IComponentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Src.Domain.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.View.Repository
+{
+    public interface IComponentRepository
+    {
+        Component SearchView<TKey>(TKey key) where TKey : IViewKey;
+
+        Component SaveView<TKey>(TKey key, Component state) where TKey : IViewKey;
+    }
+}
