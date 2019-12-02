673afa291a5974b2a0f0c6835a4d1b540e18356f
eckolo <eckolo.0722@gmail.com>
2019-12-03 01:17:51 +0900

    実テキスト操作サービス実装

---
+++ Assets/Editor/UnitTest/View/Service/TextMeshManagerTest.cs
@@ -0,0 +1,122 @@
+﻿using Assets.Src.Domain.Model.Value;
+using Assets.Src.Mock;
+using Assets.Src.View.Factory;
+using Assets.Src.View.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Editor.UnitTest.View.Service
+{
+    public class TextMeshManagerTest
+    {
+        [Test]
+        public static void UpdateText_正常系_対象有_文字更新有_位置更新有()
+        {
+            var viewState = ViewStateMock.Generate(nameof(UpdateText_正常系_対象有_文字更新有_位置更新有));
+            var positionView = new Vector2(2, -5);
+            viewState.transform.position = positionView;
+            var textOrigin = "textOrigin";
+            var positionOrigin = new Vector2(-1, 3);
+            var textUpdated = "textUpdated";
+            var positionUpdated = new Vector2(3, -12);
+            var key = new TextMeshStationeryValue(textOrigin, positionOrigin);
+
+            var result = viewState.SetText(key).UpdateText(key, textUpdated, positionUpdated);
+            result.IsNotNull();
+            result.IsSameReferenceAs(viewState);
+
+            var resultTextMesh = result.Search<TextMeshStationeryValue, TextMesh>(key);
+            resultTextMesh.IsNotNull();
+            resultTextMesh.name.Contains(textOrigin).IsTrue();
+            resultTextMesh.text.Is(textUpdated);
+            resultTextMesh.transform.position.x.Is((positionView + positionUpdated).x);
+            resultTextMesh.transform.position.y.Is((positionView + positionUpdated).y);
+            resultTextMesh.transform.localPosition.x.Is(positionUpdated.x);
+            resultTextMesh.transform.localPosition.y.Is(positionUpdated.y);
+        }
+        [Test]
+        public static void UpdateText_正常系_対象有_文字更新有_位置更新無()
+        {
+            var viewState = ViewStateMock.Generate(nameof(UpdateText_正常系_対象有_文字更新有_位置更新無));
+            var positionView = new Vector2(2, -5);
+            viewState.transform.position = positionView;
+            var textOrigin = "textOrigin";
+            var positionOrigin = new Vector2(-1, 3);
+            var textUpdated = "textUpdated";
+            var key = new TextMeshStationeryValue(textOrigin, positionOrigin);
+
+            var result = viewState.SetText(key).UpdateText(key, textUpdated);
+            result.IsNotNull();
+            result.IsSameReferenceAs(viewState);
+
+            var resultTextMesh = result.Search<TextMeshStationeryValue, TextMesh>(key);
+            resultTextMesh.IsNotNull();
+            resultTextMesh.name.Contains(textOrigin).IsTrue();
+            resultTextMesh.text.Is(textUpdated);
+            resultTextMesh.transform.position.x.Is((positionView + positionOrigin).x);
+            resultTextMesh.transform.position.y.Is((positionView + positionOrigin).y);
+            resultTextMesh.transform.localPosition.x.Is(positionOrigin.x);
+            resultTextMesh.transform.localPosition.y.Is(positionOrigin.y);
+        }
+        [Test]
+        public static void UpdateText_正常系_対象有_文字更新無_位置更新有()
+        {
+            var viewState = ViewStateMock.Generate(nameof(UpdateText_正常系_対象有_文字更新無_位置更新有));
+            var positionView = new Vector2(2, -5);
+            viewState.transform.position = positionView;
+            var textOrigin = "textOrigin";
+            var positionOrigin = new Vector2(-1, 3);
+            var positionUpdated = new Vector2(3, -12);
+            var key = new TextMeshStationeryValue(textOrigin, positionOrigin);
+
+            var result = viewState.SetText(key).UpdateText(key, positionUpdated);
+            result.IsNotNull();
+            result.IsSameReferenceAs(viewState);
+
+            var resultTextMesh = result.Search<TextMeshStationeryValue, TextMesh>(key);
+            resultTextMesh.IsNotNull();
+            resultTextMesh.name.Contains(textOrigin).IsTrue();
+            resultTextMesh.text.Is(textOrigin);
+            resultTextMesh.transform.position.x.Is((positionView + positionUpdated).x);
+            resultTextMesh.transform.position.y.Is((positionView + positionUpdated).y);
+            resultTextMesh.transform.localPosition.x.Is(positionUpdated.x);
+            resultTextMesh.transform.localPosition.y.Is(positionUpdated.y);
+        }
+        [Test]
+        public static void UpdateText_正常系_対象無()
+        {
+            var viewState = ViewStateMock.Generate(nameof(UpdateText_正常系_対象無));
+            var positionView = new Vector2(2, -5);
+            viewState.transform.position = positionView;
+            var textOrigin = "textOrigin";
+            var positionOrigin = new Vector2(-1, 3);
+            var textUpdated = "textUpdated";
+            var positionUpdated = new Vector2(3, -12);
+            var key = new TextMeshStationeryValue(textOrigin, positionOrigin);
+
+            var result = viewState.UpdateText(key, textUpdated, positionUpdated);
+            result.IsNotNull();
+            result.IsSameReferenceAs(viewState);
+
+            var resultTextMesh = result.Search<TextMeshStationeryValue, TextMesh>(key);
+            resultTextMesh.IsNull();
+        }
+        [Test]
+        public static void UpdateText_異常系_状態がNull()
+        {
+            var viewState = (ViewStateMock)null;
+            var textOrigin = "textOrigin";
+            var positionOrigin = new Vector2(-1, 3);
+            var textUpdated = "textUpdated";
+            var positionUpdated = new Vector2(3, -12);
+            var key = new TextMeshStationeryValue(textOrigin, positionOrigin);
+
+            Assert.Throws<ArgumentNullException>(() => viewState.UpdateText(key, textUpdated, positionUpdated));
+        }
+    }
+}
---
+++ Assets/Src/View/Service/TextMeshManager.cs
@@ -0,0 +1,63 @@
+﻿using Assets.Src.Domain.Model.Value;
+using Assets.Src.View.Factory;
+using Assets.Src.View.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.View.Service
+{
+    /// <summary>
+    /// <see cref="TextMesh"/>操作サービス
+    /// </summary>
+    public static class TextMeshManager
+    {
+        public static ViewState UpdateText(
+            this ViewState state,
+            TextMeshStationeryValue key,
+            string text,
+            Vector2? position = null)
+        {
+            if(state is null)
+                throw new ArgumentNullException(nameof(state));
+
+            var origin = state.Search<TextMeshStationeryValue, TextMesh>(key);
+            if(origin is null)
+                return state;
+
+            if(text is string textNonNull)
+                origin.SetText(textNonNull);
+            if(position is Vector2 positionNonNull)
+                origin.SetPosition(positionNonNull);
+            return state;
+        }
+        public static ViewState UpdateText(
+            this ViewState state,
+            TextMeshStationeryValue key,
+            Vector2 position)
+            => state.UpdateText(key, null, position);
+        static TextMesh SetPosition(
+           this TextMesh origin,
+           Vector2 position)
+        {
+            if(origin is null)
+                return origin;
+
+            origin.transform.localPosition = position;
+            return origin;
+        }
+        static TextMesh SetText(
+           this TextMesh origin,
+           string text)
+        {
+            if(origin is null)
+                return origin;
+
+            origin.text = text;
+            return origin;
+        }
+    }
+}
